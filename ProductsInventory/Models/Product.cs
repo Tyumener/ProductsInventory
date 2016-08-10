b4c28370dac83a9d89783d1de2ab50efea142728
Slava Kovtunenko <tyumener@yandex.ru>
2016-08-10 12:28:59 +0300

    Add Product Model

---
+++ ProductsInventory/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductsInventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
