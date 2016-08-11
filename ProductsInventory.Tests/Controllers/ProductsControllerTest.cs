38b2cc0306bcea59927cdd8e6051a14fac3e3afb
Slava Kovtunenko <tyumener@yandex.ru>
2016-08-11 17:38:47 +0300

    Add comments to ProductsControllerTest

--- ProductsInventory.Tests/Controllers/ProductsControllerTest.cs
+++ ProductsInventory.Tests/Controllers/ProductsControllerTest.cs
@@ -18,14 +18,16 @@ namespace ProductsInventory.Tests.Controllers
         [TestMethod]
         public async Task PostProduct_ShouldCreateNewProduct()
         {
+            // Arrange
             var mockSet = new Mock<DbSet<Product>>();
-
             var mockContext = new Mock<ProductsInventoryContext>();
             mockContext.Setup(p => p.Products).Returns(mockSet.Object);
-
             var productsController = new ProductsController(mockContext.Object);
+
+            // Act
             await productsController.PostProduct(new Product() { Id = 1, Name = "Product 1" });
 
+            //Assert
             mockSet.Verify(m => m.Add(It.IsAny<Product>()), Times.Once());
             mockContext.Verify(m => m.SaveChangesAsync(), Times.Once());
         }
@@ -33,19 +35,21 @@ namespace ProductsInventory.Tests.Controllers
         [TestMethod]
         public void GetProducts_ShouldReturnAllProducts()
         {
+            // Arrange
             var data = GetTestProducts().AsQueryable();
             var mockSet = new Mock<DbSet<Product>>();
             mockSet.As<IQueryable<Product>>().Setup(m => m.Provider).Returns(data.Provider);
             mockSet.As<IQueryable<Product>>().Setup(m => m.Expression).Returns(data.Expression);
             mockSet.As<IQueryable<Product>>().Setup(m => m.ElementType).Returns(data.ElementType);
             mockSet.As<IQueryable<Product>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
             var mockContext = new Mock<ProductsInventoryContext>();
             mockContext.Setup(c => c.Products).Returns(mockSet.Object);
-
             var service = new ProductsController(mockContext.Object);
+
+            // Act
             var products = service.GetProducts();
 
+            // Assert
             Assert.AreEqual(2, products.Count());
         }
 
@@ -53,32 +57,42 @@ namespace ProductsInventory.Tests.Controllers
         [TestMethod]
         public async Task GetProduct_ShouldReturnCorrectProduct()
         {
-            var testProducts = GetTestProducts();
-            var data = testProducts.AsQueryable();
+            // Arrange
+            var testProducts = GetTestProducts();            
             var mockSet = new Mock<DbSet<Product>>();
             var mockContext = new Mock<ProductsInventoryContext>();
             mockContext.Setup(c => c.Products).Returns(mockSet.Object);
-
             var service = new ProductsController(mockContext.Object);
+
+            // Act
             var result = await service.GetProduct(2) as OkNegotiatedContentResult<Product>;
+
+            // Assert
             mockSet.Verify(m => m.FindAsync(2), Times.Once());
         }
 
         [TestMethod]
         public async Task DeleteProduct_ShouldDeleteCorrectProduct()
         {
+            // Arrange
             var testProducts = GetTestProducts();
-            var data = testProducts.AsQueryable();
             var mockSet = new Mock<DbSet<Product>>();
             var mockContext = new Mock<ProductsInventoryContext>();
             mockContext.Setup(c => c.Products).Returns(mockSet.Object);
             mockContext.Setup(c => c.Products.FindAsync(1)).Returns(Task.FromResult(testProducts[0]));
-
             var service = new ProductsController(mockContext.Object);
+
+            // Act
             var result = await service.DeleteProduct(1) as OkNegotiatedContentResult<Product>;
+
+            // Assert
             mockContext.Verify(m => m.Products.Remove(testProducts[0]), Times.Once());
         }
 
+        /// <summary>
+        /// Generate a set of test Products
+        /// </summary>
+        /// <returns>A List with test Products</returns>
         private List<Product> GetTestProducts()
         {
             var testProducts = new List<Product>();
